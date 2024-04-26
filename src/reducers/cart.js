eb4dba635ae63be79cc94136f70f9db1217df66a
Isreal Castro <israelcastro.dev@gmail.com>
2024-04-26 13:27:56 -0500

    Agreagando localStorage

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,45 +1,67 @@
 import { CART_ACTIONS } from "./cart-actions";
 
-export const cartInitialState = [];
+export const cartInitialState =
+  JSON.parse(window.localStorage.getItem("cart")) || [];
+export const updateCartLS = (state) => {
+  window.localStorage.setItem("cart", JSON.stringify(state));
+};
 
 export function cartReducer(state, action) {
   if (action.type === CART_ACTIONS.ADD_TO_CART) {
-    const newArray = structuredClone(state);
+    updateCartLS(state);
+    const newState = structuredClone(state);
 
-    const productExistInCartIndex = newArray.findIndex(
+    const productExistInCartIndex = newState.findIndex(
       (item) => item.id === action.payload.product.id
     );
 
     if (productExistInCartIndex >= 0) {
-      newArray[productExistInCartIndex].quantity++;
-      return newArray;
+      newState[productExistInCartIndex].quantity++;
+      updateCartLS(newState);
+      return newState;
     }
 
-    return [...newArray, { ...action.payload.product, quantity: 1 }];
+    const updateState = [
+      ...newState,
+      { ...action.payload.product, quantity: 1 },
+    ];
+    
+    updateCartLS(updateState);
+    return updateState;
   }
 
   if (action.type === CART_ACTIONS.DECREASE_QUANTITY) {
-    const newArray = structuredClone(state);
+    const newState = structuredClone(state);
 
-    const productExistInCartIndex = newArray.findIndex(
+    const productExistInCartIndex = newState.findIndex(
       (item) => item.id === action.payload.id
     );
 
     if (productExistInCartIndex >= 0) {
-      newArray[productExistInCartIndex].quantity--;
-      return newArray;
+      if (newState[productExistInCartIndex].quantity > 1) {
+        // Verifica que la cantidad no sea 0 o menor
+        newState[productExistInCartIndex].quantity--;
+      }
+      updateCartLS(newState);
+      return newState;
     }
 
-    return newArray;
+    updateCartLS(newState);
+    return newState;
   }
 
   if (action.type === CART_ACTIONS.REMOVE_FROM_CART) {
-    return state.filter((item) => item.id !== action.payload.id);
+    const newArray = structuredClone(state);
+    const newState = newArray.filter((item) => item.id !== action.payload.id);
+    updateCartLS(newState);
+    return newState;
   }
 
   if (action.type === CART_ACTIONS.CLEAR_CART) {
+    updateCartLS([]);
     return [];
   }
 
+  updateCartLS(state);
   return state;
 }
