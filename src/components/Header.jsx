fe4407a3c0fb31a931a590b6a2b7d3adbf091f05
Isreal Castro <israelcastro.dev@gmail.com>
2024-04-26 11:24:43 -0500

    Ajustando flex

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,13 @@ function Header() {
   return (
     <header className="w-full flex justify-around py-4 bg-white dark:bg-slate-800 border-2 border-transparent border-b-slate-700 dark:text-slate-100 mb-5 sticky top-0 transition-colors duration-300 ease-in-out">
       <div className="flex items-center gap-x-5">
-        <h1>Carrito de compras</h1>
+        <h1 className="text-2xl font-bold">Carrito de compras</h1>
+      </div>
+
+      <div>
         <Filters />
       </div>
+
       <div
         className={`w-10 h-10 rounded-full flex items-center ${
           stateDarkMode === "light" ? "bg-yellow-500" : "bg-slate-900"
